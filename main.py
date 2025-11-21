c340dc50254a4a1830c0add698cf86d289077c8f
Evan O'Connell <eoconne6@charlotte.edu>
2025-11-21 02:20:08 -0500

    feat: savings account w interest + checking account w transfer limits

--- main.py
+++ main.py
@@ -14,6 +14,34 @@ class BankAccount:
     def print_customer_information(self):
         print("Bank Name: " + self.bank_title + ", Customer Name: " + self.name + ", Balance: " + str(self.balance) + ", Minimum Balance: " + str(self.minimum_balance))
 
+
+class SavingsAccount(BankAccount):
+    def __init__(self, name, balance, minimum_balance, interest_rate):
+        super().__init__(name, balance, minimum_balance)
+        self.interest_rate = interest_rate
+
+    def apply_interest(self):
+        interest_amount = self.balance * self.interest_rate
+        self.balance += interest_amount
+class LimitedCheckingAccount(BankAccount):
+    def __init__(self, name, balance, minimum_balance, max_transfer):
+        super().__init__(name, balance, minimum_balance)
+        self.max_transfer = max_transfer
+
+    def deposit(self, amount):
+        if amount > self.max_transfer:
+            print("Deposit is too high")
+        else:
+            self.balance += amount
+
+    def withdraw(self, amount):
+        if amount > self.max_transfer:
+            print("Withdraw amount exceeds the allowed limit.")
+        elif self.balance - amount < self.minimum_balance:
+            print("Insufficient balance.")
+        else:
+            self.balance -= amount
+
 p1 = BankAccount("John", 20000, 2000)
 p2 = BankAccount("Jeremy", 5000, 0)
 
